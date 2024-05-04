40561c1ace1f36851547351cd8f05a96ac462db0
keivan-sf <keyvan0082@gmail.com>
2024-05-04 18:11:26 +0330

    neovide

--- home.nix
+++ home.nix
@@ -3,4 +3,5 @@
   programs.home-manager.enable = true;
   xdg.configFile."i3/config".source = ./dotfiles/i3/pc;
   xdg.configFile."i3/i3-volume".source = ./dotfiles/i3/i3-volume;
+  xdg.configFile."neovide/config.toml".source = ./dotfiles/neovide/config.toml;
 }
