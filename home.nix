ec330917b8c5112fd963a205e6f1abf853b9d588
keivan-sf <keyvan0082@gmail.com>
2024-05-04 17:39:42 +0330

    .

--- home.nix
+++ home.nix
@@ -2,5 +2,5 @@
   home.stateVersion = "22.05";
   programs.home-manager.enable = true;
   xdg.configFile."i3/config".source = ./dotfiles/i3/pc;
-  xdg.configFile."i3/i3-volume.sh".source = ./dotfiles/i3/i3-volume.sh;
+  xdg.configFile."i3/i3-volume.sh".source = ./dotfiles/i3/i3-volume;
 }
