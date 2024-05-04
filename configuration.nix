83f8fcbb7979ed9c069520bbe40917831a162086
keivan-sf <keyvan0082@gmail.com>
2024-05-04 17:02:13 +0330

    Remove home manager from main file

--- configuration.nix
+++ configuration.nix
@@ -9,9 +9,6 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-  home-manager.users.keive = {
- 	xdg.configFile."i3/config".source = "i3config";
-  };
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
