0907f95f572993b7d78324039cdc9fd99b1cef3c
keivan-sf <keyvan0082@gmail.com>
2024-05-08 11:39:43 +0330

    Rewind boot loader changes

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "ahci" "nvme" "usbhid" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
@@ -17,14 +17,13 @@
     { device = "/dev/disk/by-uuid/706ee3ac-c37f-4273-adaa-60de85200cc0";
       fsType = "ext4";
     };
-  fileSystems."/efi" = {
-	device = "/dev/disk/by-uuid/78E1-2B65";
-        fsType = "autofs";
-   };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/e5472919-a923-4f95-b7c6-0672361f0948"; }
-    ];
+  fileSystems."/efi" =
+    { device = "systemd-1";
+      fsType = "autofs";
+    };
+
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -36,4 +35,3 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
-
