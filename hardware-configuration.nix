0b8661679a7b3db212e452aef8fa1fbaf28ce85c
keivan-sf <keyvan0082@gmail.com>
2024-05-08 11:56:43 +0330

    Update hardware configuration

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -18,12 +18,15 @@
       fsType = "ext4";
     };
 
-  fileSystems."/efi" =
-    { device = "systemd-1";
-      fsType = "autofs";
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/78E1-2B65";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/e5472919-a923-4f95-b7c6-0672361f0948"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
