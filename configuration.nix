96c779be11f37dda73db4621974b486a6e09b362
keivan-sf <keyvan0082@gmail.com>
2024-05-11 20:56:34 +0330

    Add xdg mime enable

--- configuration.nix
+++ configuration.nix
@@ -166,6 +166,8 @@
    "video/x-matroska" = "mpv";
   };
 
+  xdg.mime.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
