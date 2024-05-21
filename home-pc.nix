88e060214353ecf13f5f9f9b14a4163815ff12e4
keivan-sf <keyvan0082@gmail.com>
2024-05-21 15:18:12 +0330

    add pupdate for pc as well

--- home-pc.nix
+++ home-pc.nix
@@ -1,7 +1,8 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [./home-base.nix];
   xdg.configFile."i3/config".source = ./dotfiles/i3/pc;
   xdg.configFile."i3/i3-volume".source = ./dotfiles/i3/i3-volume;
   xdg.configFile."neovide/config.toml".source = ./dotfiles/neovide/config-pc.toml;
   programs.zsh.shellAliases.update = "sudo nixos-rebuild switch --flake .#pc";
+  programs.zsh.shellAliases.pupdate = let pupdatebin = pkgs.writeScriptBin "pupdate" "SYSTEM_NAME='pc'\n${builtins.readFile ./scripts/pupdate.sh}"; in "${pupdatebin}/bin/pupdate";
 }
