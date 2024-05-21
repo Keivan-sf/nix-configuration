58c24df1bbcd949969a15323ffe593aff8925292
keivan-sf <keyvan0082@gmail.com>
2024-05-21 13:06:21 +0330

    pupdate

--- home-laptop.nix
+++ home-laptop.nix
@@ -1,6 +1,7 @@
-{ ... }: {
+{  pkgs, ... }: {
   imports = [./home-base.nix];
   xdg.configFile."i3/config".source = ./dotfiles/i3/laptop;
   xdg.configFile."neovide/config.toml".source = ./dotfiles/neovide/config-laptop.toml;
   programs.zsh.shellAliases.update = "sudo nixos-rebuild switch --flake .#laptop";
+  programs.zsh.shellAliases.pupdate = let pupdatebin = pkgs.writeScriptBin "pupdate" builtins.readFile ./scripts/pupdate.sh; in "$";
 }
