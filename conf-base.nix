a81f0fcfa330a6338d10def0c121e2a7bd94e70c
keivan-sf <keyvan0082@gmail.com>
2024-08-30 19:09:29 +0330

    allow some ports

--- conf-base.nix
+++ conf-base.nix
@@ -254,7 +254,7 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 5001 5000 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
