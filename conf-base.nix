072456d2ca6a769f39c2251ee61d04f984a91429
keivan-sf <keyvan0082@gmail.com>
2024-12-03 23:01:31 +0330

    Remove sound.enable since it's not used in new version

--- conf-base.nix
+++ conf-base.nix
@@ -44,7 +44,6 @@
   hardware.sane.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
