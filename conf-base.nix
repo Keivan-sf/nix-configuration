7d237673b0f61a4ee13e67dd63119088eb8a6073
Keivan-sf <keyvan0082@gmail.com>
2025-01-16 21:53:17 +0330

    Switch back to home (i3)

--- conf-base.nix
+++ conf-base.nix
@@ -29,11 +29,11 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  #  services.xserver.displayManager.gdm.enable = true;
-  #  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.xfce.enable = true;
-  services.xserver.windowManager.i3.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+   services.xserver.displayManager.lightdm.enable = true;
+   services.xserver.desktopManager.xfce.enable = true;
+   services.xserver.windowManager.i3.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb.layout = "us,ir";
