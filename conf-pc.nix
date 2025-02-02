9f7643d121fa28b9fcfa6cfbd2b86f1bffed8746
Keivan-sf <keyvan0082@gmail.com>
2025-02-02 09:28:41 +0330

    Switch to grub

--- conf-pc.nix
+++ conf-pc.nix
@@ -20,14 +20,13 @@
   };
 
   # Bootloader.
-  #  boot.loader.systemd-boot.enable = false;
-  #  boot.loader.efi.canTouchEfiVariables = true;
-  #  boot.loader.grub.enable = true;
-  #  boot.loader.grub.device = "nodev";
-  #  boot.loader.grub.useOSProber = true;
-  #  boot.loader.grub.efiSupport = true;
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
+  #  boot.loader.systemd-boot.enable = true;
 
   # home 
   home-manager.users.keive = { imports = [ ./home-pc.nix ]; };
