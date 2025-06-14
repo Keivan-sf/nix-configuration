5b0d5a0e59e80706c923689f2852f171dfc744c0
Keivan-sf <keyvan0082@gmail.com>
2025-06-14 10:31:59 +0330

    Allow proxy ports to be open

--- conf-base.nix
+++ conf-base.nix
@@ -379,6 +379,10 @@
   networking.firewall.allowedTCPPorts = [
     443
     80
+    2080
+    2081
+    20170
+    20171
     6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
     2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
     2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
